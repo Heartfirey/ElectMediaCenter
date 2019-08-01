12d5b712b9f219eb1266f4966a3126f1da18fbdc
颜玮琦 <yanweiqi1754989931@live.com>
2019-08-01 20:58:04 +0800

    8.1

--- Settings.xaml.cs
+++ Settings.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,16 +10,29 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Data;
-using System.Drawing;
-using System.Diagnostics;
 
 namespace ElectMediaCenter_Project
 {
     /// <summary>
-    /// Setitings.xaml 的交互逻辑
+    /// Settings.xaml 的交互逻辑
     /// </summary>
-    
+    public partial class Settings : Window
+    {
+        public Settings()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public partial class Settings : Window
+    {
+        private void Visit_ProjectWeb(object sender, RoutedEventArgs e)
+        {
+            //调用系统默认的浏览器 
+            System.Diagnostics.Process.Start("https://github.com/Heartfirey/ElectMediaCenter.git");
+         }
 }
+    }
+
+    
